eae32408b844dcb25fa1f0b96ee1f5841e922436
mhijazi <mo.hijazi@manheim.com>
2016-04-14 19:45:03 -0400

    Fixed type in search_bar name

--- lib/sites/google/pages/search_page.rb
+++ lib/sites/google/pages/search_page.rb
@@ -3,7 +3,7 @@ require 'taza/page'
 
 module Google
   class SearchPage < ::Taza::Page
-    element(:serach_bar) { browser.text_field(:id, 'lst-ib') }
+    element(:search_bar) { browser.text_field(:id, 'lst-ib') }
     element(:search_button) { browser.button(:name, 'btnK') }
   end
 end
